4352040069453073a74b1937d1691db9ff14cb88
JIMIN KIM <102471138+jimin-fundamental@users.noreply.github.com>
2024-09-27 19:41:04 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,23 +1,20 @@
 def solution(board, moves):
-    answer = 0 #사라진 인형의 수
-    basket = [] 
-    #집어서 올려서 basket에 넣는 거 구현
-    len_board = len(board)
-    #return len_board
+    answer = 0
+    n = len(board)
+    stack = []
+
     for move in moves:
         move = move -1
-        for i in range(len_board):
+        for i in range(n):
             if board[i][move] != 0:
-                #인형 빼고
                 doll = board[i][move]
                 board[i][move] = 0
-                #basket 넣기 전 터지는지 확인 - pop이 오래 걸림
-                if basket and basket[-1] == doll:
-                    basket.pop()
+                if stack and stack[-1] == doll:
+                    #터뜨리기
+                    stack.pop()
                     answer += 2
                 else:
-                    basket.append(doll)
-                #loop 종료
-                break
-    
+                    stack.append(doll)
+                break                
+        
     return answer
