285ae19b71466e7b08830b6bc54bd9d32972a27a
JIMIN KIM <102471138+jimin-fundamental@users.noreply.github.com>
2024-09-28 13:28:28 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,25 +1,33 @@
 def solution(words):
-    half_p =""
-    count = {}
     odd = ""
-    answer = ""
-    for char in words:
-        if char in count:
-            count[char] += 1
+    half_p =""
+    
+    #개수 파악
+    counts = {}
+    
+    for i in range(len(words)):
+        word = words[i]
+        if word in counts:
+            counts[word] += 1
         else:
-            count[char] = 1
-    #char - 알파벳 / count[char] - 숫자
-    for char in sorted(count.keys()):
-        if count[char] % 2 ==1:
+            counts[word] = 1
+    
+    for alp in sorted(counts.keys()):
+        if counts[alp] % 2 ==1:
             if odd:
                 return "I'm Sorry Hansoo"
-            odd = char
-        half_p += char*(count[char]//2)
-        
-    answer = half_p + odd +half_p[::-1]   
+            odd = alp
+        half_p += alp * (counts[alp] // 2)
+    
+    answer = half_p + odd + half_p[::-1]
     return answer
+            
+        
 
-#a,b = tuple(map(int, input().split()))
+
+
+
+#n,k = tuple(map(int, input().split()))
 #n = int(input())
 #p = list(map(int, input().split()))
 words = input()
