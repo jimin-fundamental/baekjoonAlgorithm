4384ba367d120118db34471bae93614bc9cf7ff5
JIMIN KIM <102471138+jimin-fundamental@users.noreply.github.com>
2024-09-28 11:24:01 +0900

    [level 2] Title: N-Queen, Time: 9172.38 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,24 +1,20 @@
 def solution(n):
-    def is_safe(queen_positions, row, col):
-        # 이미 놓은 퀸들이 있는 위치와 새로 놓으려는 위치 (row, col)를 비교해서 안전한지 확인
-        for r, c in enumerate(queen_positions[:row]):
-            # 같은 열에 있거나 대각선에 위치하면 안전하지 않음
-            if c == col or abs(c - col) == abs(r - row):
+    def is_safe(loc, row, col):
+        for r,c in enumerate(loc[:row]):
+            if col == c or abs(r-row) == abs(c-col):
                 return False
         return True
-
-    def place_queens(queen_positions, row):
-        if row == n:
-            # 모든 행에 퀸을 배치했으면 성공적인 배치
-            return 1
+        
+    
+    def place_queen(loc, row):
         count = 0
+        if row == n:
+            return 1
         for col in range(n):
-            if is_safe(queen_positions, row, col):
-                # 퀸을 놓을 수 있는 자리라면 배치하고, 다음 행으로 진행
-                queen_positions[row] = col
-                count += place_queens(queen_positions, row + 1)
+            if is_safe(loc,row,col):
+                loc[row] = col
+                count += place_queen(loc, row+1)
         return count
-
-    # 각 행에 놓인 퀸의 열 위치를 저장하는 리스트
-    queen_positions = [-1] * n
-    return place_queens(queen_positions, 0)
+       
+    loc = [-1]*n
+    return place_queen(loc, 0)
