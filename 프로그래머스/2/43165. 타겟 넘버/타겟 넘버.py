c086d08d94ef50eb6a0b3aac7fcfc785f98989f2
JIMIN KIM <102471138+jimin-fundamental@users.noreply.github.com>
2025-04-05 18:29:44 +0900

    [level 2] Title: 타겟 넘버, Time: 495.02 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,18 @@
+from collections import deque
+
 def solution(numbers, target):
     answer = 0
     
-    def dfs(index, total):
-        nonlocal answer
+    def dfs(index, current_sum):
+        nonlocal answer #함수 밖에서 정의한 변수를 안에서 사용하고 싶을 때 이런 식으로 재정의
         if index == len(numbers):
-            if total == target:
+            if current_sum == target:
                 answer += 1
-            return
+            return #target이 아니어도, return해야 다음으로 넘어갈 수 있음
         
-        dfs(index+1, total + numbers[index])
-        dfs(index+1, total - numbers[index])
+        dfs(index+1, current_sum + numbers[index]) 
+        dfs(index+1, current_sum - numbers[index])
     
-    dfs(0,0)
-        
+    dfs(0, 0)
+    
     return answer
