e64a6ba558301835e58754d4cfabd3cd55948622
JIMIN KIM <102471138+jimin-fundamental@users.noreply.github.com>
2025-04-06 13:21:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.88 ms, Memory: 9.66 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,40 +1,26 @@
 from collections import deque
 
 def solution(maps):
-    n = len(maps)
-    m = len(maps[0])
+    행 = len(maps)
+    열 = len(maps[0])
+    dx = [0, 0, -1, +1]  # 상하좌우
+    dy = [+1, -1, 0, 0]
     
-    # 이동 방향: 상, 하, 좌, 우
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+    q = deque()
+    visited = [[False] * 열 for _ in range(행)]
     
-    # BFS를 위한 큐 초기화
-    queue = deque()
-    queue.append((0, 0))  # 시작점 (0,0)
-
-    while queue:
-        x, y = queue.popleft()
-        
-        # 현재 위치에서 4방향으로 이동
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            
-            # 맵 범위 벗어나면 무시
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
-            
-            # 벽이면 무시
-            if maps[nx][ny] == 0:
-                continue
-            
-            # 처음 가는 길이면 거리 저장 및 큐에 추가
-            if maps[nx][ny] == 1:
-                maps[nx][ny] = maps[x][y] + 1
-                queue.append((nx, ny))
+    q.append((0, 0))
+    visited[0][0] = True
     
-    # 상대 팀 진영에 도착했는지 확인
-    if maps[n-1][m-1] == 1:
-        return -1  # 도착 못함
-    else:
-        return maps[n-1][m-1]
+    while q:
+        x, y = q.popleft()
+        for m, n in zip(dx, dy):
+            nx = x + m
+            ny = y + n
+            if 0 <= nx < 행 and 0 <= ny < 열:
+                if maps[nx][ny] == 1 and not visited[nx][ny]:
+                    q.append((nx, ny))
+                    visited[nx][ny] = True
+                    maps[nx][ny] = maps[x][y] + 1
+                    
+    return maps[행 - 1][열 - 1] if maps[행 - 1][열 - 1] != 1 else -1
