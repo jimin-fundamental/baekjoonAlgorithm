57ff0cba40dfd378140876947aafe65c94e0f417
JIMIN KIM <102471138+jimin-fundamental@users.noreply.github.com>
2025-08-11 22:32:25 +0900

    [level 2] Title: 전화번호 목록, Time: 95.15 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,6 @@
 def solution(phone_book):
-    phone_book = set(phone_book)
-    
-    for phone in phone_book:
-        for i in range(1, len(phone)):
-            if phone[:i] in phone_book:
-                return False
+    phone_book.sort()
+    for i in range(len(phone_book) -1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
     return True
-    
