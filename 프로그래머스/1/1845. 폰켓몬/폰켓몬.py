ab7763c0bb9843afd77e3996a5a9576552681bf8
JIMIN KIM <jimin.fundamental@gmail.com>
2026-03-28 15:35:10 +0900

    [level 1] Title: 폰켓몬, Time: 1.78 ms, Memory: 9.52 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,12 +1,32 @@
+"""
+- N마리 중 N/2마리 가져감
+- 종류에 따라 번호 
+- 같은 종류라도 이를 구분
+- 최대한 많은 종류 가지고 싶음 
+- nums: N마리 폰켓몬의 종류 번호 담긴 배열 
+=> 각 번호가 몇 마리씩인지 dict로 정리 
+N/2 < len(dict) -> N/2 가짓수
+N/2 >= len(dict) -> len(dict) 가짓수
+
+"""
+
 def solution(nums):
-    dic = {}
-    numOfGet = len(nums)/2
-    for name in nums:
-        dic[name] = dic.get(name, 0) +1
-    numOfType = len(dic)
-    if numOfGet < numOfType:
-        return numOfGet
-    else:
-        return numOfType
+    answer = 0
+    N = len(nums)
     
-  
+    # dict 만들기
+    types = dict()
+    
+    for num in nums:
+        if num in types.keys():
+            types[num] += 1
+        else:
+            types[num] = 1
+    
+    # 가짓수 return
+    if N/2 < len(types):
+        answer = N/2
+    else:
+        answer = len(types)
+            
+    return answer
