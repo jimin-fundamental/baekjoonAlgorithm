4340182b71381562a9c881c4e6785b3725200fbb
JIMIN KIM <jimin.fundamental@gmail.com>
2026-03-28 18:01:42 +0900

    [level 2] Title: 피로도, Time: 28.71 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -10,36 +10,22 @@
   
   최대 8개니까 BF 가능 or DFS
 """
+from itertools import permutations
+
 def solution(k, dungeons):
     answer = 0
-    visited = [False]* len(dungeons)
     
-    def dfs(current_k, count):
-        nonlocal answer 
+    for p in permutations(dungeons):
+        current_k = k
+        count = 0
         
-        # 종료 조건
-        # 이 문제에서는 계속 answer을 기록하고
-        # 모든 던전을 다 돌았거나 더이상 갈 곳이 없으면 자연스럽게 종료함
+        for (need, spend) in p :
+            if current_k >= need:
+                count += 1
+                current_k -= spend
+            else:
+                break
         answer = max(answer, count)
         
-        if count == len(dungeons):
-            return
-        
-        # 방문 가능한 곳 모두 방문
-        for i in range(len(dungeons)):
-             if not visited[i] and current_k >= dungeons[i][0]:
-                    
-                    # 방문 처리
-                    visited[i] = True
-                    
-                    # 다음 상태로 전이
-                    dfs(current_k - dungeons[i][1], count+1)
-                    
-                    # 백트래킹 
-                    # - 다시 돌아왔을 때 다른 순서로도 돌 수 있게 방문 표시 해제
-                    visited[i] = False
-   # 초기 호출: 피로도 k, 탐험횟수 0에서 시작 
-    dfs(k, 0)
-    
     return answer
             
