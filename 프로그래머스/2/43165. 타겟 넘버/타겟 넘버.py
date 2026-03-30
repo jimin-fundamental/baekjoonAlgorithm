a7e961c2ada77d4f956c7e4c6d26c0b62413358f
JIMIN KIM <jimin.fundamental@gmail.com>
2026-03-30 21:21:45 +0900

    [level 2] Title: 타겟 넘버, Time: 637.54 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -2,17 +2,18 @@ from collections import deque
 
 def solution(numbers, target):
     answer = 0
+    q = deque([(0,0)])
     
-    def dfs(index, current_sum):
-        nonlocal answer #함수 밖에서 정의한 변수를 안에서 사용하고 싶을 때 이런 식으로 재정의
-        if index == len(numbers):
-            if current_sum == target:
-                answer += 1
-            return #target이 아니어도, return해야 다음으로 넘어갈 수 있음
+    while q:
+        idx, curr_sum = q.popleft()
         
-        dfs(index+1, current_sum + numbers[index]) 
-        dfs(index+1, current_sum - numbers[index])
-    
-    dfs(0, 0)
-    
+        # 종료 조건
+        if idx == len(numbers):
+            if curr_sum == target:
+                answer += 1
+        else:
+            # 다음 상태 전이
+            q.append((idx+1, curr_sum + numbers[idx]))
+            q.append((idx+1, curr_sum - numbers[idx]))
+                
     return answer
