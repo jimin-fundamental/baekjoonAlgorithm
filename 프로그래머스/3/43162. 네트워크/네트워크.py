2ef9837ad5e31a3c78313e52366df23a7ff24162
JIMIN KIM <jimin.fundamental@gmail.com>
2026-03-30 21:41:56 +0900

    [level 3] Title: 네트워크, Time: 0.69 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,28 +1,20 @@
-from collections import deque
-
 def solution(n, computers):
-    group = [0]*n #0이면 아직 그룹 없음, 1.2.3... 각 그룹
-    count = 0
+    answer = 0
+    visited = [False] * n
     
-    # 연결되어있는지 체크하는 함수
-    def dfs(current_node):
-        nonlocal count
-        q = deque([current_node])
-        count += 1
-        nowGroup = count #현재그룹번호
-                
-        #같은 그룹과 연결된 것끼리 DFS 수행
-        while q:
-            node = q.pop()
-            # 이미 방문한 노드면 건너뛰기
-            if group[node] != 0:
-                continue
-            group[node] = nowGroup
-            for i in range(n):
-                if computers[node][i] == 1 and group[i] ==0:
-                    q.append(i)
-    
-    for i in range(n):  # 모든 노드에 대해 dfs 시도
-        if group[i] == 0:
+    def dfs(cur):
+        nonlocal answer
+        
+        visited[cur] = True
+        
+        for next in range(n):
+            if cur != next and not visited[next] and computers[cur][next] == 1:
+                    dfs(next)
+            
+        
+    for i in range(n):
+        if not visited[i]:
             dfs(i)
-    return count
+            answer += 1
+    
+    return answer
