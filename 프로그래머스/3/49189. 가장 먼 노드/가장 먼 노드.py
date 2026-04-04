cc49e1d1114fa259b9b1ba5e899c7d2792bd88d0
JIMIN KIM <jimin.fundamental@gmail.com>
2026-04-04 12:55:39 +0900

    [level 3] Title: 가장 먼 노드, Time: 27.96 ms, Memory: 19.5 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,27 +1,33 @@
-from collections import defaultdict, deque
+from collections import deque
 
 def solution(n, edge):
-    # 1. 인접리스트로 그래프 구성
-    graph = defaultdict(list)
     
-    for x,y in edge:
-        graph[x].append(y)
-        graph[y].append(x)
-        
-    visited = [False]*(n+1)
-    distance= [0]*(n+1)
+    # 1번 노드부터 각 노드까지의 거리를 저장 
+    distances = [-1 for _ in range(n+1)]
+    distances[1] = 0
     
-    queue = deque([1])
-    visited[1] = True
-
-    while queue:
-        current = queue.popleft()
-        for neighbor in graph[current]:
-            if not visited[neighbor]:
-                visited[neighbor] = True
-                distance[neighbor] = distance[current]+1
-                queue.append(neighbor)
-    max_dist = max(distance)
-    return distance.count(max_dist)    
+    # edge 그래프(인접리스트) 만들기
+    graph = [[] for _ in range(n+1)]
+    
+    for a,b in edge:
+        graph[a].append(b)
+        graph[b].append(a)
         
-                
+    # BFS에서 방문 예정인 노드 담기 
+    q = deque([1])
+    
+    while q:
+        cur = q.popleft()
+        for neighbor in graph[cur]:
+            if distances[neighbor] == -1:
+                q.append(neighbor)
+                distances[neighbor] = distances[cur] + 1
+    
+    
+    
+    most_far = max(distances)
+    
+    return distances.count(most_far)
+        
+        
+    
