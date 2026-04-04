10e3bd85ed38c3cf09ddd2705da74b56dc1734cd
JIMIN KIM <jimin.fundamental@gmail.com>
2026-04-04 14:45:31 +0900

    [level 3] Title: 등굣길, Time: 3.55 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -6,27 +6,12 @@
 def solution(m, n, puddles):
     dp = [[0]*(m+1) for _ in range(n+1)]
     
-    dp[1][1] = 0
-    
-    # 제일 바깥쪽 위랑 왼쪽 테두리 1로 초기화
-    for y in range(2, n+1):
-        # 만약 앞이 puddle이면 거기부터 끝까지 0으로 초기화해야 함
-        if [1, y] not in puddles: #puddles는 문제에서 주는 걸로 반대로 들어가 있음 <- 주의!
-            dp[y][1] = 1
-        else:
-            break
-    for x in range(2, m+1):
-        if [x, 1] not in puddles:
-            dp[1][x] = 1
-        else:
-            break
-    
     # dp 초기화하기
-    for dy in range(2, n+1):
-        for dx in range(2, m+1):
-            if [dx, dy] in puddles:
-                dp[dy][dx] = 0
-            else:
+    for dy in range(1, n+1):
+        for dx in range(1, m+1):
+            if dy== 1 and dx == 1:
+                dp[dy][dx] = 1
+            elif [dx, dy] not in puddles:
                 dp[dy][dx] = dp[dy][dx-1]+dp[dy-1][dx]
     
     return dp[n][m] % 1000000007
